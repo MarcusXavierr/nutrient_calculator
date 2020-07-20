27cba0093160a2aaac5481ba63a43abae9145da7
Marcus Xavier <marcusxavierr123@gmail.com>
2020-07-20 15:39:32 -0300

    Create constants file

---
+++ lib/constants.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+final Color kPrimaryColor = Colors.purple.shade900;
+
+const TextStyle kCarouselValueTextStyle = TextStyle(
+  color: Colors.white,
+  fontSize: 80.0,
+);
+
+const TextStyle kCarouselGTextStyle = TextStyle(
+  color: Colors.white,
+  fontSize: 25.0,
+);
+
+const TextStyle kCarouselTextTextStyle = TextStyle(
+  color: Colors.white,
+  fontSize: 28.0,
+);
