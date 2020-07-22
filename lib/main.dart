f965373cac43b98c70389e8324c4a4a91058c4d3
Marcus Xavier <marcusxavierr123@gmail.com>
2020-07-22 10:07:26 -0300

    Refactor color

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,11 @@ import 'package:nutrients/screens/home_page.dart';
 
 void main() {
   runApp(MaterialApp(
+    title: 'Calculadora de Nutrientes',
+    theme: ThemeData(
+      primaryColor: Colors.purple.shade900,
+      accentColor: Colors.blueGrey.shade700,
+    ),
     home: HomePage(),
   ));
 }
