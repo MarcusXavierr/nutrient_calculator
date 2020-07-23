a35b3c83dc39399dd3eee4c9b0ea8bee7c2e0c09
Marcus Xavier <marcusxavierr123@gmail.com>
2020-07-23 13:03:02 -0300

    Create Model

---
+++ lib/models/food_model.dart
@@ -0,0 +1,27 @@
+class FoodModel {
+  int id;
+  String name;
+  double protein;
+  double carbo;
+  double fat;
+
+  FoodModel({this.id, this.name, this.protein, this.carbo, this.fat});
+
+  FoodModel.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    name = json['name'];
+    protein = json['protein'];
+    carbo = json['carbo'];
+    fat = json['fat'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['id'] = this.id;
+    data['name'] = this.name;
+    data['protein'] = this.protein;
+    data['carbo'] = this.carbo;
+    data['fat'] = this.fat;
+    return data;
+  }
+}
