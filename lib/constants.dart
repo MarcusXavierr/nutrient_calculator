bc0ee199c4a1b1fe41f83031ab80eac164f5ecf1
Marcus Xavier <marcusxavierr123@gmail.com>
2020-07-23 13:00:54 -0300

    Add new constants

--- lib/constants.dart
+++ lib/constants.dart
@@ -16,3 +16,29 @@ const TextStyle kCarouselTextTextStyle = TextStyle(
   color: Colors.white,
   fontSize: 28.0,
 );
+
+const TextStyle kLargeButtonTextStyle = TextStyle(
+  fontSize: 22.0,
+  fontWeight: FontWeight.bold,
+);
+
+const kTextFieldDecoration = InputDecoration(
+  hintStyle: TextStyle(
+    color: Colors.grey,
+  ),
+  hintText: 'Forgot .copyWith()',
+  contentPadding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+  border: OutlineInputBorder(
+    borderRadius: BorderRadius.all(Radius.circular(32.0)),
+  ),
+  enabledBorder: OutlineInputBorder(
+    borderSide: BorderSide(color: Colors.deepPurple, width: 1.0),
+    borderRadius: BorderRadius.all(Radius.circular(32.0)),
+  ),
+  focusedBorder: OutlineInputBorder(
+    borderSide: BorderSide(color: Colors.deepPurple, width: 2.0),
+    borderRadius: BorderRadius.all(Radius.circular(32.0)),
+  ),
+);
+
+const double kSizedBoxHeight = 10;
