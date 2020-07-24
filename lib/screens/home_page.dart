a151cae4131e9f1b0bf0c384acffd5fc22cc73af
Marcus Xavier <marcusxavierr123@gmail.com>
2020-07-24 14:20:12 -0300

    Add food data

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:nutrients/components/carousel_content.dart';
 import 'package:nutrients/components/food_tracker.dart';
+import 'package:nutrients/controllers/home_controller.dart';
+import 'package:nutrients/models/food_model.dart';
 import 'package:nutrients/screens/insert_food.dart';
 
 class HomePage extends StatefulWidget {
@@ -10,6 +12,33 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  List<FoodModel> foods;
+  HomeController _controller = HomeController();
+  Future getFoods() async {
+    foods = await _controller.getFoods();
+
+    try {
+      for (var food in foods) {
+        //print(food.toJson());
+        var foodTracker = FoodTracker(
+          counter: 1,
+          foodName: food.name,
+          fat: food.fat,
+          carbo: food.carbo,
+          protein: food.protein,
+          id: food.id,
+        );
+
+        foodTrackerList.add(foodTracker);
+      }
+    } catch (e) {
+      print(e);
+    }
+    setState(() {});
+  }
+
+  List<FoodTracker> foodTrackerList = [];
+
   List<Widget> cards = [
     CarouselContent(
       text: 'Carboidrato',
@@ -24,6 +53,13 @@ class _HomePageState extends State<HomePage> {
       text: 'Gordura',
     ),
   ];
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getFoods();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -77,24 +113,37 @@ class _HomePageState extends State<HomePage> {
                     padding: EdgeInsets.only(top: 20.0),
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.stretch,
-                      children: <Widget>[
-                        FoodTracker(
-                          counter: 3,
-                          foodName: 'Ovo',
-                        ),
-                        FoodTracker(
-                          counter: 1,
-                          foodName: 'Peito de frango',
-                        ),
-                        FoodTracker(
-                          counter: 2,
-                          foodName: 'Bife de carne bovina',
-                        ),
-                        FoodTracker(
-                          counter: 4,
-                          foodName: 'Colher de sopa de arroz integral',
-                        ),
-                      ],
+                      // children:
+                      children: foods != null
+                          ? foods
+                              .map<Widget>(
+                                (food) => FoodTracker(
+                                  counter: 1,
+                                  foodName: food.name,
+                                  fat: food.fat,
+                                  carbo: food.carbo,
+                                  protein: food.protein,
+                                ),
+                              )
+                              .toList()
+                          : <Widget>[
+                              // FoodTracker(
+                              //   counter: 1,
+                              //   foodName: 'Ovo',
+                              // ),
+                              // FoodTracker(
+                              //   counter: 1,
+                              //   foodName: 'Peito de frango',
+                              // ),
+                              // FoodTracker(
+                              //   counter: 2,
+                              //   foodName: 'Bife de carne bovina',
+                              // ),
+                              // FoodTracker(
+                              //   counter: 4,
+                              //   foodName: 'Colher de sopa de arroz integral',
+                              // ),
+                            ],
                     ),
                   ),
                 ),
