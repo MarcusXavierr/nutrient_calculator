2ebc5871364e7d12cedd3e1c6dce565b33f4c372
Marcus Xavier <marcusxavierr123@gmail.com>
2020-07-24 14:19:03 -0300

    Create alert dialog

---
+++ lib/components/alert_dialog_widget.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
+
+Alert buildAlert(String dialog, BuildContext context) {
+  return Alert(
+    title: 'Mensagem do sistema',
+    desc: dialog,
+    context: context,
+    buttons: [
+      DialogButton(
+        child: Text(
+          'Fechar',
+          style: TextStyle(color: Colors.white),
+        ),
+        color: Theme.of(context).primaryColor,
+        onPressed: () {
+          Navigator.pop(context);
+        },
+      )
+    ],
+  );
+}
