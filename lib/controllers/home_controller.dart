67d39b3376d7cfa6c33af2a4be3abe55f14097bb
Marcus Xavier <marcusxavierr123@gmail.com>
2020-07-27 12:43:09 -0300

    Refactor

--- lib/controllers/home_controller.dart
+++ lib/controllers/home_controller.dart
@@ -1,14 +1,28 @@
+import 'package:nutrients/components/food_tracker.dart';
 import 'package:nutrients/controllers/food_controller.dart';
 import 'package:nutrients/models/food_model.dart';
 
 class HomeController {
   final FoodController foodController = FoodController();
+  List<FoodTracker> foodTrackerList;
 
-  Future<List<FoodModel>> getFoods() async {
+  Future getFoods() async {
+    List<FoodTracker> foodTrackerList = [];
+
     final List<FoodModel> allFoods = await foodController.readAll();
 
     for (var food in allFoods) {
       print(food.toJson());
+      var foodTracker = FoodTracker(
+        counter: 1,
+        foodName: food.name,
+        fat: food.fat,
+        carbo: food.carbo,
+        protein: food.protein,
+        id: food.id,
+      );
+
+      foodTrackerList.add(foodTracker);
     }
 
     return allFoods;
