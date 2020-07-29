d40a4ba79c2d2737d51bd6c6ad71671b1157927e
Marcus Xavier <marcusxavierr123@gmail.com>
2020-07-29 15:48:30 -0300

    Create FoodListWidget

---
+++ lib/components/food_list_widget.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:get_it/get_it.dart';
+import 'package:nutrients/components/food_tracker.dart';
+import 'package:nutrients/controllers/home_controller.dart';
+
+class FoodListWidget extends StatelessWidget {
+  final _controller = GetIt.I.get<HomeController>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Observer(
+      builder: (BuildContext context) {
+        return Column(
+          //Todo: Quem sabe refatorar essa parte e jogar isso pra uma função ou widget
+          children: _controller.foods != null
+              ? _controller.foods.length != 0
+                  ? _controller.foods
+                      .map<Widget>(
+                        (food) => FoodTracker(
+                          counter: 1,
+                          foodName: food.name,
+                          fat: food.fat,
+                          id: food.id,
+                          carbo: food.carbo,
+                          protein: food.protein,
+                        ),
+                      )
+                      .toList()
+                  : <Widget>[Container()]
+              : <Widget>[Container()],
+        );
+      },
+    );
+  }
+}
