047a8d2b65ffdfb26eed20ef4437a0558460b55f
Marcus Xavier <marcusxavierr123@gmail.com>
2020-07-30 14:06:38 -0300

    Refactor

---
+++ lib/components/food_name_field_widget.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:nutrients/screens/edit_food_view.dart';
+
+class FoodNameFieldWidget extends StatelessWidget {
+  const FoodNameFieldWidget({
+    Key key,
+    @required this.foodName,
+    @required this.carbo,
+    @required this.id,
+    @required this.protein,
+    @required this.fat,
+  }) : super(key: key);
+
+  final String foodName;
+  final double carbo;
+  final int id;
+  final double protein;
+  final double fat;
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: GestureDetector(
+        onTap: () => Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => EditFoodView(
+              foodName: foodName,
+              carbo: carbo,
+              id: id,
+              protein: protein,
+              fat: fat,
+            ),
+          ),
+        ),
+        child: Text(
+          foodName,
+          style: TextStyle(
+            fontSize: 16,
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/components/food_tracker.dart
+++ lib/components/food_tracker.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
+import 'package:nutrients/components/food_name_field_widget.dart';
 import 'package:nutrients/components/round_button.dart';
-import 'package:nutrients/models/Controller.dart';
+import 'package:nutrients/components/warning_add_food_widget.dart';
+import 'package:nutrients/controllers/food_tracker_controller.dart';
+import 'package:nutrients/models/food_tracker_model.dart';
 import 'package:nutrients/screens/edit_food_view.dart';
-
-Controller controller = Controller();
+import 'package:nutrients/view-models/home_food_view_model.dart';
 
 class FoodTracker extends StatelessWidget {
   FoodTracker({
@@ -13,7 +16,12 @@ class FoodTracker extends StatelessWidget {
     this.fat,
     this.protein,
     this.id,
-  });
+  }) {
+    model.foodCarbo = this.carbo;
+    model.foodFat = this.fat;
+    model.foodId = this.id;
+    model.foodProtein = this.protein;
+  }
 
   final int counter;
   final String foodName;
@@ -21,6 +29,10 @@ class FoodTracker extends StatelessWidget {
   final double protein;
   final double fat;
   final int id;
+
+  FoodTrackerController _controller = FoodTrackerController();
+  FoodTrackerModel model = FoodTrackerModel();
+  final homeFoodViewModel = GetIt.I.get<HomeFoodViewModel>();
 
   @override
   Widget build(BuildContext context) {
@@ -33,54 +45,43 @@ class FoodTracker extends StatelessWidget {
               RoundIconButton(
                 icon: Icons.add,
                 onPressed: () {
-                  print(foodName);
-                  print(fat);
-                  print(protein);
-                  print(carbo);
-                  print(id);
-                  print('________');
+                  warningAddFoodWidget(
+                      context: context,
+                      addLogic: () async {
+                        DateTime today = new DateTime.now();
+                        model.datetime =
+                            "${today.year.toString()}-${today.month.toString().padLeft(2, '0')}-${today.day.toString().padLeft(2, '0')}";
+                        await _controller.create(model);
+                        await homeFoodViewModel.setAllValues();
+                        Navigator.pop(context);
+                      }).show();
                 },
               ),
               SizedBox(width: 7.0),
               RoundIconButton(
                 icon: Icons.remove,
                 onPressed: () {
-                  controller.printDogs();
+                  _controller.testing();
                 },
               ),
               SizedBox(width: 15.0),
             ],
           ),
-          Expanded(
-            child: GestureDetector(
-              onTap: () => Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => EditFoodView(
-                    foodName: foodName,
-                    carbo: carbo,
-                    id: id,
-                    protein: protein,
-                    fat: fat,
-                  ),
-                ),
-              ),
-              child: Text(
-                foodName,
-                style: TextStyle(
-                  fontSize: 16,
-                ),
-              ),
-            ),
+          FoodNameFieldWidget(
+            foodName: foodName,
+            carbo: carbo,
+            id: id,
+            protein: protein,
+            fat: fat,
           ),
           SizedBox(width: 10),
-          // Text(
-          //   counter.toString(),
-          //   style: TextStyle(
-          //     color: Colors.blueGrey.shade700,
-          //     fontSize: 20,
-          //   ),
-          // ),
+          Text(
+            counter.toString(),
+            style: TextStyle(
+              color: Colors.blueGrey.shade700,
+              fontSize: 20,
+            ),
+          ),
         ],
       ),
     );
