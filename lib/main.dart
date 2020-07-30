e320d7964d71ffc8d77c0499f7a691dfedb3e2bc
Marcus Xavier <marcusxavierr123@gmail.com>
2020-07-30 14:07:02 -0300

    Add a new singleton

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
+import 'package:nutrients/controllers/home_controller.dart';
 
 import 'package:nutrients/screens/home_page.dart';
+import 'package:nutrients/view-models/home_food_view_model.dart';
 
 void main() {
+  GetIt getIt = GetIt.I;
+  getIt.registerSingleton<HomeController>(HomeController());
+  getIt.registerSingleton<HomeFoodViewModel>(HomeFoodViewModel());
   runApp(MaterialApp(
     title: 'Calculadora de Nutrientes',
     theme: ThemeData(
