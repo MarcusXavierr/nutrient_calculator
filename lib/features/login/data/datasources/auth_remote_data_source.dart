adfdf145c65e8a783303beb0669e5c7ba5adcfec
Marcus Xavier <marcusxavierr123@gmail.com>
2020-08-27 12:19:37 -0300

    implement all functions

--- lib/features/login/data/datasources/auth_remote_data_source.dart
+++ lib/features/login/data/datasources/auth_remote_data_source.dart
@@ -1,7 +1,11 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:nutrients/core/error/exceptions.dart';
 import 'package:nutrients/core/utils/logged_user_data.dart';
 import 'package:meta/meta.dart';
 import 'package:nutrients/features/login/data/models/user_data_model.dart';
+
+typedef Future<UserCredential> _CreateOrLoginWithEmail();
 
 abstract class AuthRemoteDataSource {
   ///Call the firebase to log in with email and password
@@ -29,9 +33,62 @@ abstract class AuthRemoteDataSource {
   Stream<LoggedUserData> get user;
 }
 
-// FirebaseAuth auth = FirebaseAuth.instance;
-// get user {
-//   return auth
-//       .authStateChanges()
-//       .map((User user) => UserDataModel.fromFirebase(user));
-// }
+class AuthRemoteDataSourceImpl extends AuthRemoteDataSource {
+  final FirebaseAuth auth;
+
+  AuthRemoteDataSourceImpl({@required this.auth});
+
+  Future<LoggedUserData> _authWithEmail(
+      _CreateOrLoginWithEmail createOrLogin) async {
+    try {
+      final response = await createOrLogin();
+      return LoggedUserData.fromFirebase(response.user);
+    } catch (e) {
+      throw ServerException();
+    }
+  }
+
+  @override
+  Future<LoggedUserData> createAccountWithEmail(
+      {String email, String password}) async {
+    return await _authWithEmail(() =>
+        auth.createUserWithEmailAndPassword(email: email, password: password));
+  }
+
+  @override
+  Future<LoggedUserData> loginWithEmail({String email, String password}) async {
+    return await _authWithEmail(() =>
+        auth.signInWithEmailAndPassword(email: email, password: password));
+  }
+
+  @override
+  Future<LoggedUserData> signInWithGoogle() async {
+    try {
+      // Trigger the authentication flow
+      final GoogleSignInAccount googleUser = await GoogleSignIn().signIn();
+
+      // Obtain the auth details from the request
+      final GoogleSignInAuthentication googleAuth =
+          await googleUser.authentication;
+
+      // Create a new credential
+      final GoogleAuthCredential credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth.accessToken,
+        idToken: googleAuth.idToken,
+      );
+
+      // Once signed in, return the UserCredential
+      final UserCredential response =
+          await FirebaseAuth.instance.signInWithCredential(credential);
+
+      return LoggedUserData.fromFirebase(response.user);
+    } catch (e) {
+      throw ServerException();
+    }
+  }
+
+  @override
+  Stream<LoggedUserData> get user => auth
+      .authStateChanges()
+      .map((User user) => LoggedUserData.fromFirebase(user));
+}
