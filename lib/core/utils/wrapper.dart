4ba5bb948915aeba9c470ada1abc2f19bc874950
Marcus Xavier <marcusxavierr123@gmail.com>
2020-08-28 12:22:36 -0300

    Create wrapper

---
+++ lib/core/utils/wrapper.dart
@@ -0,0 +1,21 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:nutrients/core/utils/logged_user_data.dart';
+import 'package:nutrients/features/login/presentation/pages/create_account/create_account_page.dart';
+import 'package:nutrients/features/login/presentation/pages/login/login_page.dart';
+import 'package:nutrients/screens/home_page.dart';
+import 'package:provider/provider.dart';
+
+class Wrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final user = Provider.of<LoggedUserData>(context);
+
+    // * Return home or authenticate widget
+    if (user == null) {
+      return LoginPage();
+    } else {
+      return HomePage();
+    }
+  }
+}
