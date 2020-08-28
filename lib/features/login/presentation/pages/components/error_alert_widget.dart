56ab275a3262981852d662695b7819e9ac4ee3ea
Marcus Xavier <marcusxavierr123@gmail.com>
2020-08-28 12:24:42 -0300

    Create views

---
+++ lib/features/login/presentation/pages/components/error_alert_widget.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
+
+Alert errorAlertWidget(String dialog, BuildContext context) {
+  return Alert(
+    title: 'System Message',
+    desc: dialog,
+    context: context,
+    buttons: [
+      DialogButton(
+        child: Text(
+          'Close',
+          style: TextStyle(color: Colors.white),
+        ),
+        color: Theme.of(context).primaryColor,
+        onPressed: () {
+          Navigator.pop(context);
+        },
+      )
+    ],
+  );
+}
