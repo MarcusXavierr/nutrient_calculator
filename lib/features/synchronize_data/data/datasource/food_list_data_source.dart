b92f8bc412cca48df975521f9b293ee003c91978
Marcus Xavier <marcusxavierr123@gmail.com>
2020-09-09 15:32:01 -0300

    create data sources

---
+++ lib/features/synchronize_data/data/datasource/food_list_data_source.dart
@@ -0,0 +1,14 @@
+import 'package:nutrients/core/utils/success.dart';
+import 'package:nutrients/features/synchronize_data/data/models/food_list_data_model.dart';
+
+abstract class FoodListDataSource {
+  ///Calls the https://hasura-on-postgresql.herokuapp.com/v1/graphql endpoint.
+  ///
+  ///Throws a [ServerException] for all error codes.
+  Future<Success> uploadData(String userId);
+
+  ///Calls the https://hasura-on-postgresql.herokuapp.com/v1/graphql endpoint.
+  ///
+  ///Throws a [ServerException] for all error codes.
+  Future<FoodListDataModel> downloadData(String userId);
+}
