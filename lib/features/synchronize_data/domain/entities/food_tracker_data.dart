eb0c5f478aed26de98e200c04c73cfb2db8fda96
Marcus Xavier <marcusxavierr123@gmail.com>
2020-09-09 13:25:10 -0300

    Create entities

---
+++ lib/features/synchronize_data/domain/entities/food_tracker_data.dart
@@ -0,0 +1,7 @@
+class FoodTrackerData {
+  String userId;
+  double foodProtein;
+  double foodCarbo;
+  double foodFat;
+  String datetime;
+}
---
+++ lib/features/synchronize_data/domain/repositories/food_list_repository.dart
@@ -0,0 +1,10 @@
+import 'package:dartz/dartz.dart';
+import 'package:nutrients/core/error/failure.dart';
+import 'package:nutrients/core/utils/success.dart';
+import 'package:nutrients/features/synchronize_data/domain/entities/food_list_data.dart';
+
+abstract class FoodListRepository {
+  Future<Either<Failure, Success>> uploadFoodListData(String userId);
+
+  Future<Either<Failure,FoodListData>> downloadFoodListData(String userId);
+}
