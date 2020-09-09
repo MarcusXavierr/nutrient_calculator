b41415ec20ed902722cb80c9d59d62b39936244b
Marcus Xavier <marcusxavierr123@gmail.com>
2020-09-09 16:28:33 -0300

    Create food tracker repository implementation

---
+++ lib/features/synchronize_data/data/repositories/food_tracker_data_repository_impl.dart
@@ -0,0 +1,34 @@
+import 'package:nutrients/core/error/exceptions.dart';
+import 'package:nutrients/core/network/network_info.dart';
+import 'package:nutrients/features/synchronize_data/data/datasource/food_tracker_data_source.dart';
+import 'package:nutrients/features/synchronize_data/domain/entities/food_list_data.dart';
+import 'package:nutrients/core/utils/success.dart';
+import 'package:nutrients/core/error/failure.dart';
+import 'package:dartz/dartz.dart';
+import 'package:meta/meta.dart';
+import 'package:nutrients/features/synchronize_data/domain/repositories/food_list_data_repository.dart';
+
+class FoodTrackerDataRepositoryImpl extends FoodListDataRepository {
+  final NetworkInfo networkInfo;
+  final FoodTrackerDataSource dataSource;
+
+  FoodTrackerDataRepositoryImpl(
+      {@required this.networkInfo, @required this.dataSource});
+  @override
+  Future<Either<Failure, FoodListData>> downloadFoodListData(String userId) {
+    // TODO: implement downloadFoodListData
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<Failure, Success>> uploadFoodListData(String userId) async {
+    if (!await networkInfo.isConnected) {
+      return Left(NetworkFailure());
+    }
+    try {
+      return Right(await dataSource.uploadData(userId));
+    } on ServerException {
+      return Left(ServerFailure());
+    }
+  }
+}
