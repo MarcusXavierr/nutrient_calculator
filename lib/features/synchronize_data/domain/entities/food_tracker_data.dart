9ee4012a5dd1448bf85fe135d7dc7c8a45df1a06
Marcus Xavier <marcusxavierr123@gmail.com>
2020-09-09 14:07:33 -0300

    create constructor

--- lib/features/synchronize_data/domain/entities/food_tracker_data.dart
+++ lib/features/synchronize_data/domain/entities/food_tracker_data.dart
@@ -1,7 +1,17 @@
+import 'package:meta/meta.dart';
+
 class FoodTrackerData {
   String userId;
   double foodProtein;
   double foodCarbo;
   double foodFat;
   String datetime;
+
+  FoodTrackerData({
+    @required this.foodCarbo,
+    @required this.foodFat,
+    @required this.foodProtein,
+    @required this.userId,
+    @required this.datetime,
+  });
 }
