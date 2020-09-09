52fe25900278b2380a368939063bfc7aec554984
Marcus Xavier <marcusxavierr123@gmail.com>
2020-09-09 14:08:43 -0300

    create abstract repositories

---
+++ lib/features/synchronize_data/domain/repositories/food_tracker_data_repository.dart
@@ -0,0 +1,11 @@
+import 'package:dartz/dartz.dart';
+import 'package:nutrients/core/error/failure.dart';
+import 'package:nutrients/core/utils/success.dart';
+import 'package:nutrients/features/synchronize_data/domain/entities/food_tracker_data.dart';
+
+abstract class FoodTrackerDataRepository {
+  Future<Either<Failure, Success>> uploadFoodTrackerData(String userId);
+
+  Future<Either<Failure, FoodTrackerData>> downloadFoodTrackerData(
+      String userId);
+}
