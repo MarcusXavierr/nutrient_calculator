52207f9141edc9f962afd1c55351ecd261cecf33
Marcus Xavier <marcusxavierr123@gmail.com>
2020-09-10 10:46:07 -0300

    create download functions

--- lib/features/synchronize_data/data/repositories/food_list_data_repository_impl.dart
+++ lib/features/synchronize_data/data/repositories/food_list_data_repository_impl.dart
@@ -8,6 +8,8 @@ import 'package:dartz/dartz.dart';
 import 'package:meta/meta.dart';
 import 'package:nutrients/features/synchronize_data/domain/repositories/food_list_data_repository.dart';
 
+typedef Future<Success> _DownloadOrUpload();
+
 class FoodListDataRepositoryImpl extends FoodListDataRepository {
   final NetworkInfo networkInfo;
   final FoodListDataSource dataSource;
@@ -17,18 +19,23 @@ class FoodListDataRepositoryImpl extends FoodListDataRepository {
     @required this.dataSource,
   });
   @override
-  Future<Either<Failure, FoodListData>> downloadFoodListData(String userId) {
-    // TODO: implement downloadFoodListData
-    throw UnimplementedError();
+  Future<Either<Failure, Success>> downloadFoodListData(String userId) async {
+    return await _downloadOrUploadData(() => dataSource.downloadData(userId));
   }
 
   @override
   Future<Either<Failure, Success>> uploadFoodListData(String userId) async {
+    return await _downloadOrUploadData(() => dataSource.uploadData(userId));
+  }
+
+  Future<Either<Failure, Success>> _downloadOrUploadData(
+    _DownloadOrUpload function,
+  ) async {
     if (!await networkInfo.isConnected) {
       return Left(NetworkFailure());
     }
     try {
-      return Right(await dataSource.uploadData(userId));
+      return Right(await function());
     } on ServerException {
       return Left(ServerFailure());
     }
