e69684b60ea026ae7f0c7a23d97eda1c0c45e409
Marcus Xavier <marcusxavierr123@gmail.com>
2020-09-12 14:26:21 -0300

    Modify db conn

--- lib/repositories/db_connection.dart
+++ lib/repositories/db_connection.dart
@@ -2,22 +2,22 @@ import 'package:nutrients/constants.dart';
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 
-class DBConnection {
-  void _createDatabase(Database db) async {
-    await db.execute('''CREATE TABLE foods (id INTEGER PRIMARY KEY, 
+void createDatabase(Database db) async {
+  await db.execute('''CREATE TABLE foods (id INTEGER PRIMARY KEY, 
         name TEXT NOT NULL,carbo REAL NOT NULL,protein REAL NOT NULL, 
         fat REAL NOT NULL)''');
 
-    await db.execute('''CREATE TABLE tracker (id INTEGER PRIMARY KEY, 
+  await db.execute('''CREATE TABLE tracker (id INTEGER PRIMARY KEY, 
     food_id INTEGER NOT NULL,food_carbo REAL NOT NULL, 
     food_protein REAL NOT NULL, food_fat REAL NOT NULL,datetime TEXT NOT NULL)''');
-  }
+}
 
+class DBConnection {
   Future<Database> open() async {
     final Future<Database> database = openDatabase(
       join(await getDatabasesPath(), DatabaseName),
       onCreate: (db, version) {
-        return _createDatabase(db);
+        return createDatabase(db);
       },
       version: 1,
     );
