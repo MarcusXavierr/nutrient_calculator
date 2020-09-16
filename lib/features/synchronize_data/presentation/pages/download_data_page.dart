329d7812c27b5076697cf52263c56c11a68c1662
Marcus Xavier <marcusxavierr123@gmail.com>
2020-09-16 13:34:18 -0300

    Create views

--- lib/features/synchronize_data/presentation/pages/download_data_page.dart
+++ lib/features/synchronize_data/presentation/pages/download_data_page.dart
@@ -1,7 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:get_it/get_it.dart';
+import 'package:nutrients/controllers/home_controller.dart';
+import 'package:nutrients/features/login/presentation/pages/components/error_alert_widget.dart';
+import 'package:nutrients/features/synchronize_data/presentation/mobx/download_data_controller.dart';
 import 'package:nutrients/features/synchronize_data/presentation/pages/components/sync_button_widget.dart';
+import 'package:nutrients/view-models/home_food_view_model.dart';
 
 class DownloadDataPage extends StatelessWidget {
+  final DownloadDataController _controller =
+      GetIt.I.get<DownloadDataController>();
+
+  final _homeController = GetIt.I.get<HomeController>();
+  final _homeViewModel = GetIt.I.get<HomeFoodViewModel>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -11,26 +23,50 @@ class DownloadDataPage extends StatelessWidget {
       ),
       body: Container(
         margin: EdgeInsets.symmetric(vertical: 25.0, horizontal: 15.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: <Widget>[
-            SyncButtonWidget(
-              title: 'Download food list from cloud',
-              text:
-                  'This button will download your list of foods that are in the cloud and use it to replace local data, meaning your current foods will be deleted and replaced with the foods that you previously added to the cloud.',
-              function: () {
-                print('download food list');
-              },
-            ),
-            SyncButtonWidget(
-              title: 'Download your calorie intake data',
-              text:
-                  'This button downloads the calorie data that you have ingested that is in the cloud and uses that data to replace the current local data, that is, the local data will be erased and the data that is in the cloud will be used to replace it.',
-              function: () {
-                print('Download food tracker');
-              },
-            ),
-          ],
+        child: Observer(
+          builder: (_) {
+            if (_controller.isLoading) {
+              return Center(child: CircularProgressIndicator());
+            } else {
+              return Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  SyncButtonWidget(
+                    title: 'Download food list from cloud',
+                    text:
+                        'This button will download your list of foods that are in the cloud and use it to replace local data, meaning your current foods will be deleted and replaced with the foods that you previously added to the cloud.',
+                    function: () async {
+                      await _controller.downloadFoodList();
+                      if (_controller.isLoaded) {
+                        await errorAlertWidget(
+                          _controller.message,
+                          context,
+                          color: _controller.color,
+                        ).show();
+                      }
+                      await _homeController.getFoods();
+                    },
+                  ),
+                  SyncButtonWidget(
+                    title: 'Download your calorie intake data',
+                    text:
+                        'This button downloads the calorie data that you have ingested that is in the cloud and uses that data to replace the current local data, that is, the local data will be erased and the data that is in the cloud will be used to replace it.',
+                    function: () async {
+                      await _controller.downloadFoodTracker();
+                      if (_controller.isLoaded) {
+                        await errorAlertWidget(
+                          _controller.message,
+                          context,
+                          color: _controller.color,
+                        ).show();
+                      }
+                      await _homeViewModel.setAllValues();
+                    },
+                  ),
+                ],
+              );
+            }
+          },
         ),
       ),
     );
--- lib/features/synchronize_data/presentation/pages/upload_data_page.dart
+++ lib/features/synchronize_data/presentation/pages/upload_data_page.dart
@@ -1,7 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:get_it/get_it.dart';
+import 'package:nutrients/controllers/home_controller.dart';
+import 'package:nutrients/features/login/presentation/pages/components/error_alert_widget.dart';
+import 'package:nutrients/features/synchronize_data/presentation/mobx/upload_data_controller.dart';
 import 'package:nutrients/features/synchronize_data/presentation/pages/components/sync_button_widget.dart';
+import 'package:nutrients/view-models/home_food_view_model.dart';
 
 class UploadDataPage extends StatelessWidget {
+  final _controller = GetIt.I.get<UploadDataController>();
+  final _homeController = GetIt.I.get<HomeController>();
+  final _homeViewModel = GetIt.I.get<HomeFoodViewModel>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -11,26 +20,49 @@ class UploadDataPage extends StatelessWidget {
       ),
       body: Container(
         margin: EdgeInsets.symmetric(horizontal: 15.0, vertical: 25.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: <Widget>[
-            SyncButtonWidget(
-              title: 'Upload your food list',
-              text:
-                  'This button goes up all the food you have registered for the cloud',
-              function: () {
-                print('upload food list');
-              },
-            ),
-            SyncButtonWidget(
-              title: 'Upload your calorie intake data',
-              text:
-                  'This button uploads the data for all calories you have eaten to the cloud',
-              function: () {
-                print('upload food tracker');
-              },
-            ),
-          ],
+        child: Observer(
+          builder: (_) {
+            if (_controller.isLoading) {
+              return Center(child: CircularProgressIndicator());
+            }
+            return Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                SyncButtonWidget(
+                  title: 'Upload your food list',
+                  text:
+                      'This button goes up all the food you have registered for the cloud',
+                  function: () async {
+                    await _controller.uploadFoodList();
+                    if (_controller.isLoaded) {
+                      await errorAlertWidget(
+                        _controller.message,
+                        context,
+                        color: _controller.color,
+                      ).show();
+                    }
+                    await _homeController.getFoods();
+                  },
+                ),
+                SyncButtonWidget(
+                  title: 'Upload your calorie intake data',
+                  text:
+                      'This button uploads the data for all calories you have eaten to the cloud',
+                  function: () async {
+                    await _controller.uploadFoodTracker();
+                    if (_controller.isLoaded) {
+                      await errorAlertWidget(
+                        _controller.message,
+                        context,
+                        color: _controller.color,
+                      ).show();
+                    }
+                    await _homeViewModel.setAllValues();
+                  },
+                ),
+              ],
+            );
+          },
         ),
       ),
     );
